ed6d2de8e432f47cf673556446654b6496763a17
mahmud10cool <mutashi@gmail.com>
2025-05-10 18:21:15 -0500

    A lot of new updates

--- FinalProjectSupportingFiles/FinalProjectSupportingFiles/evaluateConstraints.m
+++ FinalProjectSupportingFiles/FinalProjectSupportingFiles/evaluateConstraints.m
@@ -14,6 +14,7 @@ function[c, ceq] = evaluateConstraints(x)
     
     %% Compute the inequality and equality constraints
 
+    %% Constraint 1
     rs = 180; % stator radius [mm]
     dsp = 4; % stator tooth tip dimension [mm]
     dso = 2; % stator tooth tip thickness [mm]
@@ -21,53 +22,35 @@ function[c, ceq] = evaluateConstraints(x)
     
     rsi = rs - dsy - dst - dsp; % stator inner bore radius [mm]
     rm = rsi - delta; % magnet tip radius [mm]
-    c1 = Omega*2*pi/60*rm/1000 - 175; % tip speed constraint
-    % disp('c1')
-    % disp(c1)
-    
+    c1 = Omega*((2*pi)/60)*(rm*1e-3) - 175; % tip speed constraint
+
+    %% Constraint 2
     Kcu = 0.4;
     dw=0.324861*0.0254*exp(-0.115942*18); % wire diameter [m]
     Sc = 0.25*pi*dw^2*1e6;
     alpha_c = pi/6;
     zQ = floor((Kcu/(2*Sc))*(alpha_c*(rs-dsy)*dst - wst*dst - 0.5*alpha_c*dst^2)); % number of turns
-    % disp(z_Q)
-    % Sc = 0.82355; % cross sectional area [mm^2]
-    % zQ = round(Kcu/Sc/2*(pi/12*dst^2 + (pi/6*ri - wst)*dst)); % number of turns
-    % 
+
 
     I_hat = 5*Sc*sqrt(2); % peak current [A]
     z_C = 4;
     k_w1 = 1; % fundamanetal winding factor
     r = rsi - delta/2; % airgap mid radius [mm]
-    c2 = (3*zQ*I_hat*z_C*k_w1)/(pi*sqrt(2)*r*1e-3) - 80*1000; % rms current...
-    % loading constraint
-    % disp('c2')
-    % disp(c2)
-    c3 = (wst/2) - rsi*sin(deg2rad(ast/2)); % tooth geometry constraint
-    %c3 =-1; % you have already given us a code for that
 
-    % disp('rsi')
-    % disp(rsi)
-    % disp('wst')
-    % disp(wst)
-    % minima_ast = 2*asin(wst/2/rsi);
-    % disp('minima ast')
-    % disp(minima_ast)
-    % 
-    % ast_actual = ast*pi/180;
-    % disp('actual ast')
-    % disp(ast_actual)
-    % disp('c3')
-    % disp(c3)
+    % RMS current
+    c2 = (3*zQ*I_hat*z_C*k_w1)/(pi*sqrt(2)*r*1e-3) - 80*1e3; 
 
-    c4 = -(rs - dsy - dst - dsp - delta - dm); % valid geometry check 
-    % disp('c4')
-    % disp(c4)
+    %% Constraint 3
+    % Tooth geometry constraint
+    c3 = (wst/2) - rsi*sin(deg2rad(ast/2));
+
+    %% Constraint 4
+    % Valid geometry check
+    c4 = -(rs - dsy - dst - dsp - delta - dm); 
 
     c = [c1;c2;c3;c4];
     
-    ceq = checkToothTip(rs/1000, dso/1000, dsp/1000, dst/1000, wst/1000, dsy/1000, ast*pi/180);
-    % disp('ceq')
-    % disp(ceq)
+    %% Constraint 5
+    ceq = checkToothTip(rs/1000, dso/1000, dsp/1000, dst/1000, wst/1000, dsy/1000, ast*(pi/180));
 
 end
---
+++ FinalProjectSupportingFiles/FinalProjectSupportingFiles/testDesign1.m
@@ -0,0 +1,81 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is a test script for HW 3 Part 2). This is an incomplete script and
+% you need to complete it as per the instructions provided.
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Initialization
+clear all;
+close all;
+addpath('C:\femm42\mfiles'); % This is the FEMM path. You may need to modify this to the installation directory of FEMM on your PC.
+clc;
+
+steps = 12;
+
+%% Stator Dimensions
+dimensions.stator.dso = 5;           %[mm]
+dimensions.stator.dsp = 7.5;         %[mm]
+dimensions.stator.dst = 57;          %[mm]
+dimensions.stator.dsy = 35;          %[mm]
+dimensions.stator.alpha_st = 55;     %[degree]
+dimensions.stator.wst = 50;          %[mm]
+dimensions.outerRadius = 200;        %[mm]
+dimensions.delta = 3.5;              %[mm] Airgap length
+dimensions.length = 600;             %[mm] Axial length
+Q = 12;
+
+%% Rotor Dimensions
+dimensions.rotor.dri = 50;         %[mm] 
+dimensions.rotor.dm=8;            %[mm] 
+dimensions.rotor.dmp=6;            %[mm]
+dimensions.rotor.alpha_m = 80;     %[deg]
+dimensions.rotor.OuterRadius = dimensions.outerRadius - (dimensions.stator.dsp + dimensions.stator.dst + dimensions.stator.dsy + dimensions.delta);
+
+%% Materials 
+materials.coil.name = '18 AWG';
+materials.coil.conductivity = 5.77e7; %Copper conductivity in S/m
+materials.coil.AWG = 18;
+materials.iron.name = 'M19_29Ga';
+materials.iron.ch = 0.0186;  % Hysteresis loss coefficient for M19-29Ga in (Watt/(kg * T^2 * Hz^2)
+materials.iron.ce = 6.8874e-5; % Eddy loss Current coefficient for M19-29Ga in (Watts/(kg * T^2 * Hz)
+materials.iron.sf = 0.91;  % Lamination stacking factor (nondimensional)
+materials.iron.density = 7650; %Mass density of M19 29Ga in [kg/m^3]
+materials.magnet.name = 'Recoma35E';
+materials.magnet.conductivity = 1.11e6; %Magnet conductivity in S/m
+
+%% Define the winding structure and add parameters
+dwire=0.324861*0.0254*exp(-0.115942*materials.coil.AWG); % wire diameter in meters as a function of AWG
+Acond = 0.25*pi*dwire^2;
+iHat = 5*1e6*Acond*sqrt(2);
+A_hat = 25e3;
+N = Q/3; % Because it is a double layer winding
+r_airgap = 1e-3*(dimensions.rotor.OuterRadius);
+
+zQ = round((pi*A_hat*r_airgap)/(3*N*0.866*iHat)); % This is 261 turns
+% zQ = 252; % This is just to match the given torque values.
+winding.layers = 2; 
+winding.topSlots.circuit = [{'U'}, {'V'}, {'W'},{'U'}, {'V'}, {'W'}];
+winding.topSlots.zQ = zQ.*[1, 1, 1, 1, 1, 1]; % Positive number for conductors that go into the page
+winding.bottomSlots.zQ = zQ.*[-1, -1, -1, -1, -1, -1];
+winding.bottomSlots.circuit = [{'W'}, {'U'}, {'V'},{'W'}, {'U'}, {'V'}];
+winding.y = 1;
+
+%% Add parameters to the settings structure
+settings.RPM = 10000; % Rated Speed in RPM
+settings.iHat = iHat;  % Peak current [A]
+settings.phi = 30;   % Angle in [deg] (The current linkage was found in ALE15 for the same specifications, the peak is at 30 degrees, but there are two poles so 15)
+settings.lowestHarmonic = 2; %Lowest Harmonic (n) for which winding factor |Kw,n| is non-zero; The max value it can take is p
+
+%% Define the number of pole pairs
+p = 2;
+
+%% Analysis
+
+for i = 1:length(steps)
+    settings.steps = steps(i); % Number of steps the design must be evaluated at; Keep this an even number;
+
+    [designEval, length] = evaluateDesign(materials, dimensions, p, winding, settings, ratedTorque);
+    
+    fprintf('The avg. torque is %1.3f [Nm]; the expected value is 398.058[Nm]\n', designEval.torque.average);
+    fprintf('The torque ripple is %1.3f; the expected value is 0.130\n', designEval.torque.ripple);
+    fprintf('The Efficiency is %1.3f percent; the expected value is 86.635 percent\n', designEval.efficiency);
+end
